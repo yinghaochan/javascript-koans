77682ba74ce7c27c5c8d85f94ed9a445989d5799
Ying Hao Chan <yinghao@outlook.com>
2015-09-08 09:22:42 +0800

    Enlightenment! Also, _.chain(x) has a type error but _(x).chain() works???

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -62,14 +62,17 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
-
-    expect(233168).toBe(FILL_ME_IN);
+    var sum =     _.range(1,1000).reduce(function(memo, num, index){
+      if(num % 3 == 0 || num % 5 == 0){
+        return memo + num;
+      }else{return memo;}
+    },0);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -82,15 +85,19 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
+     _(products).chain()
+    .map(function(x){return x.ingredients;})
+    .flatten()
+    .each(function(x){ingredientCount[x] = (ingredientCount[x] || 0) + 1;})
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
